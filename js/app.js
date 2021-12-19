99de707ecb83580d64c825309bdc5d3a05b8419a
FMorinaDev <francomorina@outlook.com>
2021-12-19 11:35:15 -0300

    Enviando mail con mensaje confirmacion

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const email =document.querySelector('#email');
 const asunto =document.querySelector('#asunto'); 
 const mensaje =document.querySelector('#mensaje'); 
 const formulario = document.querySelector('#enviar-mail');
+const er =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 aventListener();
 
 function aventListener() {
@@ -12,6 +13,7 @@ function aventListener() {
      email.addEventListener('blur',validarFormulario);
      asunto.addEventListener('blur',validarFormulario);
      mensaje.addEventListener('blur',validarFormulario);
+     formulario.addEventListener('submit',enviarEmail);
 }
 
 //funciones
@@ -31,8 +33,6 @@ function validarFormulario(e) {
         e.target.classList.remove('border-red-500');
         e.target.classList.add('border','border-green-500');
         if (e.target.type === 'email') {
-            const er =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
             if ( er.test(e.target.value)) {
                 
             }else{
@@ -45,6 +45,14 @@ function validarFormulario(e) {
         e.target.classList.remove('border-green-500');
         e.target.classList.add('border','border-red-500');
         mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (er.test(email.value)&& asunto.value !==''&&mensaje.value!=='') {
+        btnEnviar.disabled=false;
+        btnEnviar.classList.remove('cursos-not-allowed','opacity-50');     
+    }else{
+        btnEnviar.disabled=true;
+        btnEnviar.classList.add('cursos-not-allowed','opacity-50');
     }
 }
 function mostrarError(mensaje) {
@@ -59,4 +67,20 @@ function mostrarError(mensaje) {
         error.remove();
     }
     formulario.appendChild(mensajeError);
+}
+function enviarEmail(e) {
+    e.preventDefault();
+     const spinner = document.querySelector('#spinner');
+     //mostramos el spinner
+     spinner.style.display='flex';
+     //Luego de 3 segundos ocultamos el spinner
+     setTimeout(() => {
+         spinner.style.display='none';
+         //Mensaje que dice que se envio correctamente
+         const parrafo=document.createElement('p');
+         parrafo.textContent='El mensaje se envio correctamente';
+         parrafo.classList.add('text-center','my-10','p-2','bg-green-500','text-white','font-bold','uppercase');
+         //Insertamos el mensaje arriba de los botones
+         formulario.insertBefore(parrafo,spinner);
+     }, 3000);
 }
